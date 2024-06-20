249b682dd3805cfa6ec98167f113e64bbef98b64
leopham <hongthaipro@gmail.com>
2024-06-20 09:28:25 +0700

    add logo

---
+++ src/components/Logo.tsx
@@ -0,0 +1,16 @@
+import { PiggyBank } from "lucide-react";
+import Link from "next/link";
+import React from "react";
+
+function Logo() {
+  return (
+    <Link href="/" className="flex items-center gap-2">
+      <PiggyBank className="stroke h-11 w-11 stroke-lime-500 stroke-[1.5]" />
+      <p className="bg-gradient-to-r from-lime-400 to-green-500 bg-clip-text text-3xl font-bold leading-tight tracking-tighter text-transparent">
+        Budget Tracker
+      </p>
+    </Link>
+  );
+}
+
+export default Logo;
